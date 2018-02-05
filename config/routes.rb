e77e947b7f8be40845896bf7894e925382ee7b12
ashanker90 <ashanker@iloan.com>
2018-02-05 15:44:11 -0600

    Basic devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   get 'ember-items', to: 'portfolios#ember'
